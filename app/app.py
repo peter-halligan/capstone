2e0374723b6625444c013459c8ffaad099e7d721
peter <pha@tiatechnology.com>
2021-01-16 07:19:11 +0100

    refactor: remove unneeded COPY app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, json
 
 app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
-    return 'Hello World!'
+    return  json.dumps({'hello': 'world'})
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8081, debug=True)
